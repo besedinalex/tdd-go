c8e42defef6d36cb580079dc530917313be05c90
Alexey Besedin <besedinalexey@gmail.com>
2022-02-14 18:10:29 +0300

    Added pointers

---
+++ pointers/wallet.go
@@ -0,0 +1,39 @@
+package pointers
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+var ErrInsufficientFunds = errors.New("an attempt to withdraw more than there is")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
