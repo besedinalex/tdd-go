e75fa53fcd27005aab160828cd676e516b1c998a
Alexey Besedin <besedinalexey@gmail.com>
2022-02-15 10:46:26 +0300

    Added mocking examples

---
+++ mocking/countdown.go
@@ -0,0 +1,28 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(duration time.Duration)
+}
+
+func (c ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, "Go!")
+}
---
+++ mocking/countdown_test.go
@@ -0,0 +1,91 @@
+package mocking
+
+import (
+	"bytes"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+)
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+const write = "write"
+const sleep = "sleep"
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func TestCountdown(t *testing.T) {
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
+		Countdown(buffer, spySleeper)
+
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("sleeps after each print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{write, sleep, write, sleep, write, sleep, write}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("got %v\nwant%v", spySleepPrinter.Calls, want)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("want to sleep %v, got %v", sleepTime, spyTime)
+	}
+}
+
+func ExampleCountdown() {
+	sleeper := &ConfigurableSleeper{1 * time.Millisecond, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+	// Output: 3
+	//2
+	//1
+	//Go!
+}
